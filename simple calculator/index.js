2d386e2def845990a022756eb2fe2608b4c77ad5
sadiaAziz15 <161435344+sadiaAziz15@users.noreply.github.com>
2024-04-14 05:34:35 -0700

    Add files via upload

---
+++ simple calculator/index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtraction", "multiplication", "division"],
+    },
+]);
+//conditional statement
+if (asnwer.operator === "addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operator === "substraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operator === "multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operator === "division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
+else {
+    console.log("please select valid operators");
+}
